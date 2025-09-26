f37108cb543a94a536288f66e076936dd8217f66
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-26 12:53:04 -0300

    feat: add global error handler for validation and server errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes.ts";
+import z, { ZodError } from "zod";
+import { env } from "./env/index.ts";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: "Validation error.",
+            issues: z.treeifyError(error),
+        });
+    }
+
+    if (env.NODE_ENV != "production") {
+        console.error(error);
+    } else {
+        // TODO: Hewe we should log to an external tool
+    }
+
+    return reply.status(500).send({ message: "Internal server error." });
+});
