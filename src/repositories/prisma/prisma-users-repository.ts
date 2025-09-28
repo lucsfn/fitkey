f664dc1cb198b21c888a25375f3f03a25ff793cd
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-28 19:13:23 -0300

    feat: add get user profile use case and related error handling

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,13 @@
 import { prisma } from "@/lib/prisma.ts";
-import type { Prisma } from "@prisma/client";
+import type { Prisma, User } from "@prisma/client";
 import type { UsersRepository } from "./users-repository.ts";
 
 export class PrismaUsersRepository implements UsersRepository {
+
+    async findById(id: string): Promise<User | null> {
+        // TODO
+    }
+
     async findByEmail(email: string) {
         const user = await prisma.user.findUnique({
             where: {
