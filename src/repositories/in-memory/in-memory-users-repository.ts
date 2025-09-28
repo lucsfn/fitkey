027b8b03f61377d2d1afd9a009d1b63dcd85f59c
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-28 19:38:37 -0300

    refactor: use random UUIDs in in-memory user repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import type { Prisma, User } from "@prisma/client";
 import type { UsersRepository } from "../prisma/users-repository.ts";
+import { randomUUID } from "node:crypto";
 
 export class InMemoryUsersRepository implements UsersRepository {
     public items: User[] = [];
@@ -26,7 +27,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = {
-            id: "12345678",
+            id: randomUUID(),
             name: data.name,
             email: data.email,
             password_hash: data.password_hash,
