4ab3902141fadf3e5c2088870ed7de0ff02e99b5
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-29 16:18:10 -0300

    feat: enforce 20-minute validation rule for check-ins

--- src/services/validate-check-in.spec.ts
+++ src/services/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository.ts";
-import { expect, describe, it, beforeEach, afterEach } from "vitest";
+import { expect, describe, it, beforeEach, afterEach, vi } from "vitest";
 import { ValidateCheckInUseCase } from "./validate-check-in.ts";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error.ts";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error.ts";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -11,11 +12,11 @@ describe("Validate Check-in Use Case", () => {
         checkInsRepository = new InMemoryCheckInsRepository();
         sut = new ValidateCheckInUseCase(checkInsRepository);
 
-        // vi.useFakeTimers()
+        vi.useFakeTimers();
     });
 
     afterEach(() => {
-        // vi.useRealTimers()
+        vi.useRealTimers();
     });
 
     it("should be able to validate the check-in", async () => {
@@ -40,5 +41,24 @@ describe("Validate Check-in Use Case", () => {
                 checkInId: "inexistent-check-in-id",
             })
         ).rejects.toBeInstanceOf(ResourceNotFoundError);
+    });
+
+    it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+        vi.setSystemTime(new Date(2025, 0, 1, 15, 30));
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id: "gym-01",
+            user_id: "user-01",
+        });
+
+        const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+        vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+        await expect(() =>
+            sut.execute({
+                checkInId: createdCheckIn.id,
+            })
+        ).rejects.toBeInstanceOf(LateCheckInValidationError);
     });
 });
