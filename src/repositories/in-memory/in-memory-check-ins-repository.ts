8a6d0f78e2fa68ac8e713ed77d50b55507c52d78
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-29 16:00:49 -0300

    feat: add validate check-in use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -26,6 +26,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
         return checkInOnSameDate;
     }
 
+    async findById(id: string) {
+        const checkIn = this.items.find((item) => item.id === id);
+
+        if (!checkIn) {
+            return null;
+        }
+
+        return checkIn;
+    }
+
     async findManyByUserId(userId: string, page: number) {
         return this.items
             .filter((item) => item.user_id === userId)
@@ -48,6 +58,18 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
         };
 
         this.items.push(checkIn);
+
+        return checkIn;
+    }
+
+    async save(checkIn: CheckIn) {
+        const checkInIndex = this.items.findIndex(
+            (item) => item.id === checkIn.id
+        );
+
+        if (checkInIndex >= 0) {
+            this.items[checkInIndex] = checkIn;
+        }
 
         return checkIn;
     }
