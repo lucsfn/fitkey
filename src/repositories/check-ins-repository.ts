8d8ba2172b89339d8df3cf0c84e5611e0587b869
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-29 13:09:58 -0300

    feat: add method for retrieving user check-in metrics to repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,7 +1,8 @@
 import type { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
-     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
-     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
-     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+    findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+    findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+    countByUserId(userId: string): Promise<number>;
+    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
 }
