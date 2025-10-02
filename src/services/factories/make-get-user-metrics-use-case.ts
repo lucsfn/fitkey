5280344f9c3799a03d44c5368addbf142fd46a73
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 14:11:37 -0300

    fix: correct typo in user metrics factory function name

--- src/services/factories/make-get-user-metrics-use-case.ts
+++ src/services/factories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository.ts";
 import { GetUserMetricsUseCase } from "../get-user-metrics.ts";
 
-export function makeGetUserMetricsUserCase() {
+export function makeGetUserMetricsUseCase() {
     const checkInsRepository = new PrismaCheckInsRepository();
     const useCase = new GetUserMetricsUseCase(checkInsRepository);
 
