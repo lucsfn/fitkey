ae7932eef7c5c1a79605a682431c57c7c26d9290
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 10:17:38 -0300

    feat: apply JWT middleware to profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.ts";
 import { authenticate } from "./controllers/authenticate.ts";
 import { profile } from "./controllers/profile.ts";
+import { verifyJWT } from "./middlewares/verify-jwt.ts";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/users", register);
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
     app.post("/sessions", authenticate);
 
     // Authenticated routes
-    app.get("/me", profile);
+    app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
