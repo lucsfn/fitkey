7ad743ac1e54b5f523b3ca397737eedba9c6ad9e
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 14:46:12 -0300

    refactor: reorder gym creation parameters for consistency

--- src/http/controllers/gyms/create.ts
+++ src/http/controllers/gyms/create.ts
@@ -17,7 +17,7 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
         }),
     });
 
-    const { title, description, latitude, longitude, phone } =
+    const { title, description, phone, latitude, longitude } =
         createGymBodySchema.parse(request.body);
 
     const createGymUseCase = makeCreateGymUseCase();
@@ -25,9 +25,9 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     await createGymUseCase.execute({
         title,
         description,
+        phone,
         latitude,
         longitude,
-        phone,
     });
 
     return reply.status(201).send();
