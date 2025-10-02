521ba5ab8a3f2a34ec7204d49d9a75e394660d57
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 13:38:08 -0300

    feat: register gym routes with authentication

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,11 +1,15 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt.ts";
 import type { FastifyInstance } from "fastify";
-
+import { search } from "./search.ts";
+import { nearby } from "./nearby.ts";
+import { create } from "./create.ts";
 
 export async function gymsRoutes(app: FastifyInstance) {
     // All routes below this hook will call the jwt middleware
-    app.addHook('onRequest', verifyJWT)
+    app.addHook("onRequest", verifyJWT);
 
-    
+    app.get("/gyms/search", search);
+    app.get("/gyms/nearby", nearby);
 
+    app.post("/gyms", create);
 }
