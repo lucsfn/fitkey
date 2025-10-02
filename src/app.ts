18f294a59ebcb2fd3a8e03a68faaf19346a57047
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 00:50:56 -0300

    feat: add JWT authentication support using @fastify/jwt

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes.ts";
 import z, { ZodError } from "zod";
 import { env } from "./env/index.ts";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+});
 
 app.register(appRoutes);
 
