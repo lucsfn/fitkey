1bddc6051dbba11e8f58adbf50cc8e770c9eb49b
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-10-02 16:17:50 -0300

    feat: add token refresh endpoint

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from "./register.ts";
 import { authenticate } from "./authenticate.ts";
 import { profile } from "./profile.ts";
 import { verifyJWT } from "@/http/middlewares/verify-jwt.ts";
+import { refresh } from "./refresh.ts";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post("/users", register);
 
     app.post("/sessions", authenticate);
+
+    app.patch("/token/refresh", refresh);
 
     // Authenticated routes
     app.get("/me", { onRequest: [verifyJWT] }, profile);
